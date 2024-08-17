db589b10f43ff9a9df0b3780f0391c0a57b55c94
tomchaccom <dreamkms2014@gmail.com>
2024-08-17 15:59:53 +0900

    피드백 반영하기

--- week02/src/main/java/com/example/week02/ProductRepository.java
+++ week02/src/main/java/com/example/week02/ProductRepository.java
@@ -4,6 +4,5 @@ import com.example.week02.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    //
+    // 풀리퀘 확인
 }
-    
